478e5f8285f9178eb76c8c1cfd8a5c067af4964b
Ali Hassan <alihassan17122002@gmail.com>
2022-01-28 10:14:10 -0800

    initial commit

---
+++ app/src/main/java/app/netlify/dev_ali_hassan/hafizalquran/MainActivity.kt
@@ -0,0 +1,11 @@
+package app.netlify.dev_ali_hassan.hafizalquran
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
