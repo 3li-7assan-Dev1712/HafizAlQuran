2966d9f205df898e6ef9f2fc4ffb65ccb0190ff1
Ali Hassan <alihassan17122002@gmail.com>
2022-01-29 09:17:13 -0800

    created and designed the first fragment to display the list of Suar in Quran.

---
+++ app/src/main/java/app/netlify/dev_ali_hassan/hafizalquran/ui/allsurahs/AllSurahsFragment.kt
@@ -0,0 +1,12 @@
+package app.netlify.dev_ali_hassan.hafizalquran.ui.allsurahs
+
+import androidx.fragment.app.Fragment
+import app.netlify.dev_ali_hassan.hafizalquran.R
+
+/**
+ * This fragment will be responsible for displaying all Surahs in Quran.
+ * All the work (business logic will be delegated to a dedicated view model).
+ */
+class AllSurahsFragment: Fragment(R.layout.all_surahs_fragment) {
+
+}
