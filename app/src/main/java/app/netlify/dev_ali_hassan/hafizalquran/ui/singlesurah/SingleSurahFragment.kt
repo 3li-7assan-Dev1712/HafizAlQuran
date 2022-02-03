c63353d4269b78239121a81d473606aafdec36bd
Ali Hassan <alihassan17122002@gmail.com>
2022-02-02 22:22:23 -0800

    created the SingleSurahFragment.kt and added to nav_graph.xml

---
+++ app/src/main/java/app/netlify/dev_ali_hassan/hafizalquran/ui/singlesurah/SingleSurahFragment.kt
@@ -0,0 +1,12 @@
+package app.netlify.dev_ali_hassan.hafizalquran.ui.singlesurah
+
+import androidx.fragment.app.Fragment
+import app.netlify.dev_ali_hassan.hafizalquran.R
+
+/*
+* The Fragment will show a single surah pages, it is opened when the user
+* select a surah they want to save, revision.
+* */
+class SingleSurahFragment : Fragment(R.layout.single_surah_fragment) {
+
+}
