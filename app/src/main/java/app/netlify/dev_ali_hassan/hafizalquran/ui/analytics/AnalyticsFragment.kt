feb73796fdaacff44ae7d24c1f02f134c4d41a78
Ali Hassan <alihassan17122002@gmail.com>
2022-04-21 08:51:38 -0800

    added layouts to the main fragments to display via bottom navigation view.

---
+++ app/src/main/java/app/netlify/dev_ali_hassan/hafizalquran/ui/analytics/AnalyticsFragment.kt
@@ -0,0 +1,12 @@
+package app.netlify.dev_ali_hassan.hafizalquran.ui.analytics
+
+import androidx.fragment.app.Fragment
+import app.netlify.dev_ali_hassan.hafizalquran.R
+import dagger.hilt.android.AndroidEntryPoint
+
+/*
+* The Fragment will show a single surah pages, it is opened when the user
+* selects a surah they want to save, revision.
+* */
+@AndroidEntryPoint
+class AnalyticsFragment : Fragment(R.layout.analytics_fragment)
---
+++ app/src/main/java/app/netlify/dev_ali_hassan/hafizalquran/ui/readers/SkilledActiveReadersFragment.kt
@@ -0,0 +1,12 @@
+package app.netlify.dev_ali_hassan.hafizalquran.ui.readers
+
+import androidx.fragment.app.Fragment
+import app.netlify.dev_ali_hassan.hafizalquran.R
+import dagger.hilt.android.AndroidEntryPoint
+
+/*
+* The Fragment will show a single surah pages, it is opened when the user
+* selects a surah they want to save, revision.
+* */
+@AndroidEntryPoint
+class SkilledActiveReadersFragment : Fragment(R.layout.skillful_active_readers_fragment)
